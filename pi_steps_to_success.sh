e1d6f4258c2987850c525e8218bb407012d16799
Yoseph Maguire <yoseph.maguire@microsoft.com>
2019-04-19 17:07:26 -0700

    update steps

--- pi_steps_to_success.sh
+++ pi_steps_to_success.sh
@@ -1,24 +1,43 @@
-sudo sed -i '19i\systemctl start ssh' /etc/rc.local
+
+if ! grep -Fxq "systemctl start ssh" /etc/rc.local
+then
+    echo 'Enabling automatic start of ssh on bootup...'
+    sudo sed -i '19i\systemctl start ssh' /etc/rc.local
+    # code if not found
+fi
+
 sudo apt-get update -y
 sudo apt-get install -y vim git build-essential pkg-config git cmake libssl-dev uuid-dev valgrind
 
-export CURL_ROOT=/home/pi/curl_install
+if ! grep -Fxq "CURL_ROOT" ~/.bashrc
+then
+    echo 'Enabling automatic start of ssh on bootup...'
+    echo 'export CURL_ROOT=/home/pi/curl_install' >> ~/.bashrc 
+    # code if not found
+fi
+
+if ! grep -Fxq "LD_LIBRARY_PATH" ~/.bashrc
+then 
+    echo 'export LD_LIBRARY_PATH'
+    echo 'export LD_LIBRARY_PATH=$CURL_ROOT/lib' >> ~/.bashrc
+fi
+
+echo 'Setting variables by calling bashrc'
+source ~/.bashrc
+
+# BUILD AND INSTALL NEW CURL
 wget https://curl.haxx.se/download/curl-7.64.1.tar.gz
 mkdir $CURL_ROOT
 mkdir curl_source
 tar -C curl_source -xzvf curl-7.64.1.tar.gz
 cd curl_source/curl-7.64.1/
-./configure --prefix=$CURL_ROOT --with-ssl --with-zlib
+./configure --prefix=$CURL_ROOT --with-ssl --with-zlib --disable-shared --enable-static 
 make -j
 make install
-vim /etc/rc.local
 
 
-export LD_LIBRARY_PATH=$CURL_ROOT/lib/
-
+# BUILD THE SDK
 cd ~
 cd azure-iot-sdk-c
 mkdir cmake && cd cmake
-ls
-cmake -Drun_unittests=ON -Drun_e2e_tests=ON -DCMAKE_BUILD_TYPE=Debug ..
 cmake -Drun_unittests=ON -Drun_e2e_tests=ON -DCMAKE_BUILD_TYPE=Debug -DCURL_LIBRARY=$CURL_ROOT/lib/libcurl.a -DCURL_INCLUDE_DIR=$CURL_ROOT/include  ..
