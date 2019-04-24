25625393a5ecb0878e14db711d5ea81d4a9325cf
Yoseph Maguire <yoseph.maguire@microsoft.com>
2019-04-23 23:08:31 -0700

    Update sdk_build.sh

--- devops/sdk_build.sh
+++ devops/sdk_build.sh
@@ -5,16 +5,15 @@ mkdir cmake
 cd cmake 
 [ $? -eq 0 ] || { echo "cd cmake failed "; exit 1; }
 
-cmake -D C_SDK_ROOT=/sdk -DCMAKE_TOOLCHAIN_FILE=toolchain.cmake \
-    -Duse_prov_client:BOOL=OFF \
+cmake -DCMAKE_TOOLCHAIN_FILE=toolchain.cmake \
+    -Duse_prov_client:BOOL=OFF 
     -DCMAKE_INSTALL_PREFIX=${TOOLCHAIN_PREFIX} \ 
     -DCMAKE_BUILD_TYPE=Debug \
     -Drun_e2e_tests:BOOL=ON \
     -Drun_unittests=ON \
-    -D use_amqp=ON -D use_mqtt=ON -D use_http=ON \
     ..
 
 [ $? -eq 0 ] || { echo "cmake failed"; exit 1; }
 
-cmake --build .
+cmake --build . -j
 [ $? -eq 0 ] || { echo "make failed"; exit 1; }
