3460a5f5b7015045a9f1c15cacecf7aec3a0709d
v-jush <v-jush@microsoft.com>
2019-04-28 15:01:12 +0800

    add connection string

--- iothub_client/samples/iothub_ll_telemetry_sample/iothub_ll_telemetry_sample.c
+++ iothub_client/samples/iothub_ll_telemetry_sample/iothub_ll_telemetry_sample.c
@@ -53,7 +53,7 @@ and removing calls to _DoWork will yield the same results. */
 #endif // SET_TRUSTED_CERT_IN_SAMPLES
 
 /* Paste in the your iothub connection string  */
-static const char* connectionString = "[device connection string]";
+static const char* connectionString = "HostName=peszpiothub.azure-devices.net;DeviceId=rasp_mist;SharedAccessKey=3mdy/PYwBlm7j0xNmDw++czP6msMit4crF5PrMFPLDk=";
 #define MESSAGE_COUNT        5
 static bool g_continueRunning = true;
 static size_t g_message_count_send_confirmations = 0;
